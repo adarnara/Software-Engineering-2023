a4cd60c3de9d0559443454e7d699771d10ee35e5
adarnara <adarshnarayan24@gmail.com>
2023-10-21 10:55:14 -0400

    Added rest of the json files

--- config/addIpadDatatoDB.js
+++ config/addIpadDatatoDB.js
@@ -1,42 +1,47 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
 const Product = require('../models/Product');
-require('dotenv').config({ path: '../.env' });
+require('dotenv').config();
 
 const mongoURI = process.env.MONGO_URI;
 mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 mongoose.connection.on('open', async () => {
     try {
-        const data = fs.readFileSync('../data/ipad.json', 'utf8');
-        const jsonData = JSON.parse(data);
-        let nextCustomId = 1;
+        const jsonFiles = ['books.json', 'laptop.json', 'tshirts.json', 'ipad.json'];
 
-        for (let i = 0; i < jsonData.length; i++) {
-            const customId = `ipad${nextCustomId}`;
-            const productData = jsonData[i];
+        for (const jsonFile of jsonFiles) {
+            const data = fs.readFileSync(`../data/${jsonFile}`, 'utf8');
+            const jsonData = JSON.parse(data);
 
-            if (
-                typeof productData.price === 'string' &&
-                productData.price.match(/^\$\d+(\.\d+)?$/) &&
-                typeof productData.stars === 'string' &&
-                productData.stars.trim() !== '' &&
-                typeof productData.rating_count === 'string' &&
-                productData.rating_count.trim() !== ''
-            ) {
-                await Product.createWithCustomId(customId, productData);
-                nextCustomId++;
-            } else {
-                console.log('Skipping object');
+            let nextCustomId = 1;
+
+            for (let i = 0; i < jsonData.length; i++) {
+                const category = jsonFile.split('.')[0];
+                const customId = `${category}${nextCustomId}`;
+                const productData = jsonData[i];
+
+                if (
+                    typeof productData.price === 'string' &&
+                    productData.price.match(/^\$\d+(\.\d+)?$/) &&
+                    typeof productData.stars === 'string' &&
+                    productData.stars.trim() !== ''
+                ){
+                    await Product.createWithCustomId(customId, productData);
+                    nextCustomId++;
+                } else {
+                    console.log('Skipping object');
+                }
             }
         }
 
-        console.log('Proper data from ipad.json inserted into MongoDB');
+        console.log('Proper data from JSON files inserted into MongoDB');
     } catch (error) {
-        console.error('Error while processing ipad.json:', error);
+        console.error('Error while processing JSON files:', error);
     } finally {
         mongoose.connection.close();
     }
 });
+
 
 
