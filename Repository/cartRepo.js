b67750469bd2703809186f42c1905430cc46c5c9
Tommy Chu <tommy99chu@gmail.com>
2023-11-01 16:08:28 -0400

    expand cartRepo functionality

--- Repository/cartRepo.js
+++ Repository/cartRepo.js
@@ -13,8 +13,28 @@ class ShoppingCart {
     }
     
     async getUserCartHistory(email) {
-        return await shoppingCart.find({email: email, purchaseTime: !null});
+        return await shoppingCart.find({email: email, purchaseTime: {"$ne": null}});
     }
+
+    async addProductToCart(email, product_id, quantity) {
+        const currCart = await shoppingCart.findOne({email: email, purchaseTime: null});
+        const productToAdd = await cartProduct.create({parent_cart: currCart, product_id: product_id, quantity: quantity});
+        return await productToAdd.save();
+    }
+
+    // async getProductsFromCart(cart_id) {
+    //     return await cartProduct.find({parent_cart: cart_id});
+    // }
+
+    async getProductsFromUser(email) {
+        return await shoppingCart.findOne({email: email, purchaseTime: null}).then(res => 
+                cartProduct.find({parent_cart: res})
+            )
+
+    }
+   
+
+
 }
 
 module.exports = new ShoppingCart();
--- models/shoppingCartTester.js
+++ models/shoppingCartTester.js
@@ -46,30 +46,30 @@ const cartRepo = require('../Repository/cartRepo')
 connectDB();
 
 
-const testShoppingCart = new shoppingCartCollection(
-    {
-        email: "johndoes23@gmail.com", 
-        purchaseTime: Date.now()
-    }
-)
+// const testShoppingCart = new shoppingCartCollection(
+//     {
+//         email: "johndoes23@gmail.com", 
+//         purchaseTime: Date.now()
+//     }
+// )
 
-// console.log(testShoppingCart);
-testShoppingCart.save();
-
-
+// // console.log(testShoppingCart);
+// testShoppingCart.save();
 
 
-cartProductCollection.find({})
-    .then((cartProducts) => {
-        console.log('Data retrieved from MongoDB: ');
-        console.log(cartProducts);
 
-        mongoose.connection.close();
-    })
-    .catch((err) => {
-        console.error('Error querying data: ' + err);
-        mongoose.connection.close();
-    });
+
+// cartProductCollection.find({})
+//     .then((cartProducts) => {
+//         console.log('Data retrieved from MongoDB: ');
+//         console.log(cartProducts);
+
+//         mongoose.connection.close();
+//     })
+//     .catch((err) => {
+//         console.error('Error querying data: ' + err);
+//         mongoose.connection.close();
+//     });
 
 // const testCartProduct1 = new cartProductCollection(
 //     {
@@ -83,8 +83,35 @@ cartProductCollection.find({})
 
 
 
-// cartRepo.getUserCurrentCart("johndoes23@gmail.com").then((res) => console.log(res))
+// const currentCart = cartRepo.getUserCurrentCart("johndoes23@gmail.com").then((res) => console.log(res))
+// console.log(currentCart)
+
+// fetch("localhost")
+
+const url = `http://localhost:3000/cart/add`
+const request = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body:
+        JSON.stringify(
+            {
+                quantity: 1,
+                product_id: '1232131',
+                email: "johndoes23@gmail.com"
+            }
+        )
+};
+
+
+// fetch(url, request).then(res => console.log("fetch: ",  res)).then(data => console.log("fetch: ",  data));
+
+// cartRepo.addProductToCart("johndoes23@gmail.com", 123124, 69).then(res => console.log(res))
 
 // cartRepo.createEmptyCart("johndoes23@gmail.com").then((res) => console.log(res))
 
 // cartRepo.getUserCartHistory("johndoes23@gmail.com").then((res) => console.log(res))
+
+cartRepo.getProductsFromUser("johndoes23@gmail.com").then(res => console.log(res));
+
