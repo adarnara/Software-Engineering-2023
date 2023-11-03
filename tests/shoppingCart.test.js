0c64d60a8246db3610e67b3c8efacc0844accc28
Tommy Chu <tommy99chu@gmail.com>
2023-11-03 09:34:42 -0400

    more shoppingcart tests

--- tests/shoppingCart.test.js
+++ tests/shoppingCart.test.js
@@ -7,10 +7,6 @@ const server = require('../server.js');
 const cartRepo = require('../Repository/cartRepo.js');
 const cartModel = require('../models/shoppingCart.js');
 
-
-
-
-
 beforeAll(async () => {
   await db.connect();
 });
@@ -26,36 +22,60 @@ afterAll(async () => {
   await new Promise(resolve => server.close(resolve));
 });
 
-  describe('Creating new cart for user email', () => {
-    const email = "twc44@scarletmail.rutgers.edu"
+describe('Cart operatens', () => {
+  const email = "twc44@scarletmail.rutgers.edu"
 
-    it('first test', async () => {
+  it('new cart for user', async () => {
 
-      const cart = await cartRepo.createEmptyCart(email);
-      console.log(cart.email)
-      expect(cart.email).toBe(email)
-      expect(cart.purchaseTime).toBe(null)
-      expect(cart.products).toStrictEqual([])
-      expect(cart.numShipped).toBe(null)
-      expect(cart.totalPrice).toBe(0)
+    const cart = await cartRepo.createEmptyCart(email);
+    console.log(cart.email)
+    expect(cart.email).toBe(email)
+    expect(cart.purchaseTime).toBe(null)
+    expect(cart.products).toStrictEqual([])
+    expect(cart.numShipped).toBe(null)
+    expect(cart.totalPrice).toBe(0)
 
-    })
+  })
 
-    const product_id = "19243"
+  const product_id = "19243"
 
-    it("add product", async() => {
-      const cart = await cartRepo.createEmptyCart(email);
+  it("add product", async () => {
+    const cart = await cartRepo.createEmptyCart(email);
 
-      const item = await cartRepo.addProductToCart(email, product_id, 13)
-      const cartProducts = await cartRepo.getProductsFromCartObject(cart);
-      // console.log("brugga ", cartProducts)
-      // console.log("brugga2 ", cartProducts[0].parent_cart.toString())
-      expect(cartProducts[0].parent_cart.toString()).toBe(cart._id.toString())
-      expect(cartProducts[0].product_id).toBe(product_id)
-      expect(cartProducts[0].quantity).toBe(13)
+    const item = await cartRepo.addProductToCart(email, product_id, 13)
+    const cartProducts = await cartRepo.getProductsFromCartObject(cart);
+    expect(cartProducts[0].parent_cart.toString()).toBe(cart._id.toString())
+    expect(cartProducts[0].product_id).toBe(product_id)
+    expect(cartProducts[0].quantity).toBe(13)
+  })
 
-    })
+  it("change product quantity and delete from cart", async () => {
+    const cart = await cartRepo.createEmptyCart(email);
+    const item = await cartRepo.addProductToCart(email, product_id, 13)
+    await cartRepo.changeProductQuantity(email, product_id, 12)
+    let cartProducts = await cartRepo.getProductsFromCartObject(cart);
+
+    expect(cartProducts[0].parent_cart.toString()).toBe(cart._id.toString())
+    expect(cartProducts[0].product_id).toBe(product_id)
+    expect(cartProducts[0].quantity).toBe(12)
+
+    await cartRepo.deleteProductFromCart(product_id, cart._id);
+    cartProducts = await cartRepo.getProductsFromCartObject(cart);
+
+    expect(cartProducts[0]).toBe(undefined)
 
 
 
-  });
+  })
+
+
+
+
+
+
+
+
+
+});
+
+
