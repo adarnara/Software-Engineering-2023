6a03a8ad6067020134adb7023e24d6a8748933ba
mmogi03 <116332585+mmogi03@users.noreply.github.com>
2023-12-05 10:54:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const connectDB = require('./config/db');
-const startElasticsearch = require('./config/elasticsearch');
-const copyMongoDataToElasticsearch = require('./config/importData');
+// const startElasticsearch = require('./config/elasticsearch');
+// const copyMongoDataToElasticsearch = require('./config/importData');
 const http = require('http');
 const url = require('url');
 const PORT = process.env.PORT || 3000;
@@ -21,10 +21,10 @@ const searchRouter = require("./routes/searchRoute");
 
 
 connectDB();
-startElasticsearch();
-setTimeout(() => {
-    copyMongoDataToElasticsearch();
-}, 30000);
+// startElasticsearch();
+// setTimeout(() => {
+//     copyMongoDataToElasticsearch();
+// }, 30000);
 
 
 const server = http.createServer(async (request, response) => {
