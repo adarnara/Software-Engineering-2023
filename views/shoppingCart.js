4aa43a32ed08ebd8b3c4d5796969bf08b1b5d10b
ALIENHZ1 <91748187+ALIENHZ1@users.noreply.github.com>
2023-12-07 13:16:25 -0500

    updated navbar

--- views/shoppingCart.js
+++ views/shoppingCart.js
@@ -417,3 +417,56 @@ document.addEventListener("DOMContentLoaded", async () => {
   }
   window.changeImage = changeImage;
 });
+
+function toProfile() {
+  checkToken().then(function redirect(data) {
+      const role = data.role;
+      if (role === "Seller") {
+          window.location.href = "/views/profilePageSeller.html";
+      } else if (role === "Member") {
+          window.location.href = "/views/profilePageMember.html";
+      } else {
+          console.error("Unknown role:", role);
+      }
+  });
+}
+
+function logout() {
+  removeJwtToken();
+  window.location.href = "/views/landingPage.html";
+}
+
+function setup() {
+  const token = getJwtToken();
+
+  if (token) {
+      const profileButton = document.createElement("button");
+      profileButton.className = "go-to-page-button";
+      profileButton.innerHTML = '<img src="../public/Images/profile.png" alt="Profile" />';
+      profileButton.onclick = function() {
+          toProfile();
+      };
+
+      document.getElementById("shopping-icon").innerHTML = 
+      `<a href="http://127.0.0.1:5500/views/shoppingCartHistory.html">
+      <img src="../public/Images/shoppingCartHistory.png" alt="shoppingCart" />
+      <span style="font-weight: bold; font-size: 20px"></span>
+    </a>` + 
+          '<a href="/views/shoppingCart.html" style="text-decoration: none; color: inherit;">' +
+          '<img src="../public/Images/shoppingCartIcon.png" alt="shoppingCart" />' +
+          '<span style="font-weight: bold; font-size: 20px;"></span>' +
+          '</a>';
+
+      document.getElementById("shopping-icon").appendChild(profileButton);
+
+      const logoutButton = document.createElement("button");
+      logoutButton.className = "go-to-page-button";
+      logoutButton.innerHTML = '<img src="/public/Images/image-button-two.png" alt="Logout" />';
+      logoutButton.onclick = logout;
+
+      document.getElementById("shopping-icon").appendChild(logoutButton);
+  }
+}
+
+// Set up the web page
+setup();
